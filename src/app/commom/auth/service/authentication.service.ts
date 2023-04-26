88e5fe96baed1b28156f2b135a282b3ddeab7dcb
XandRules <alexandresilva58@gmail.com>
2023-04-26 12:01:38 -0300

    criado serviço de autenticacao

--- src/app/commom/auth/service/authentication.service.ts
+++ src/app/commom/auth/service/authentication.service.ts
@@ -1,12 +1,58 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
+import { BehaviorSubject, Observable, map } from 'rxjs';
+import { HttpBaseService } from 'src/app/shared/base/http-base.service';
+import { Login } from '../models/login';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthenticationService {
+export class AuthenticationService extends HttpBaseService {
 
   // subjects
   // bahaviorSubjects
 
-  constructor() { }
+  private subjectUsuario: BehaviorSubject<any> = new BehaviorSubject(null);
+  private subjectLogin: BehaviorSubject<any> = new BehaviorSubject(false);
+  
+
+  constructor(protected override readonly injector: Injector) {
+    super(injector)
+   }
+
+
+   login(login: Login): Observable<any>{
+
+      return this.httpPost('authentication', login).pipe(
+        map((resposta) => {
+
+          sessionStorage.setItem('token', resposta.token);
+          this.subjectUsuario.next(resposta.user);
+          this.subjectLogin.next(true);
+          return resposta.user;
+        }),
+      )
+
+   }
+
+   sair(){
+    sessionStorage.removeItem('token');
+    this.subjectUsuario.next(null);
+    this.subjectLogin.next(false);
+   }
+
+   usuarioEstaLogado(): Observable<any>{
+
+    const token = sessionStorage.getItem('token');
+
+    if(token) {
+      this.subjectLogin.next(true);
+    }
+
+    return this.subjectLogin.asObservable();
+      
+    }
+
+  obterUsuario(){
+    this.subjectUsuario.asObservable();
+  }  
 }
