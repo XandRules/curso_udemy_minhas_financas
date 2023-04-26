a7122d73c8e7929073bc5075def8ad243ac4b955
XandRules <alexandresilva58@gmail.com>
2023-04-25 21:03:00 -0300

    Ajustado tela de login

--- src/app/commom/auth/components/login/login.component.ts
+++ src/app/commom/auth/components/login/login.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../service/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+
+  constructor(private authenticationService: AuthenticationService,
+    private router: Router){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+
+  login(){
+
+  }
+
+  logout(){
+
+  }
+
+  // Guardas de Rotas
+
+  // CanActivate  CanLoad
+
+  // CanDeactivate -> casdastro
+
+  // Interceptors -> users -> token -> 
+
 
 }
