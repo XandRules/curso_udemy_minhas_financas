3ed5873ab286daca4d1ed822c159798646f6300a
XandRules <alexandresilva58@gmail.com>
2023-04-28 19:37:37 -0300

    Finalizado projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { ToolbarComponent } from './commom/componentes/toolbar/toolbar.component';
 import { MaterialModule } from './shared/material/material.module';
 
@@ -12,6 +12,7 @@ import { LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 import { AuthGuard } from './commom/auth.guard';
+import { AuthInterceptor } from './commom/auth.interceptor';
 
 registerLocaleData(localePt);
 
@@ -30,6 +31,7 @@ registerLocaleData(localePt);
   ],
   providers: [
     AuthGuard,
+    { provide : HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     {
       provide: LOCALE_ID,
       useValue: 'pt-PT'
--- src/app/features/entradas/service/entradas.service.ts
+++ src/app/features/entradas/service/entradas.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Injector } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpBaseService } from 'src/app/shared/base/http-base.service';
 import { Entrada } from '../models/entrada.model';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
