425fe71d17882b502310f2b047d9d455ea67957e
XandRules <alexandresilva58@gmail.com>
2023-06-19 18:18:40 -0300

    aula segundo testes  concluida

--- src/app/features/dashboard/components/dashboard/dashboard.component.spec.ts
+++ src/app/features/dashboard/components/dashboard/dashboard.component.spec.ts
@@ -1,31 +1,78 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
-import {MockBuilder, MockModule, MockProvider} from 'ng-mocks';
+import {MockBuilder, MockInstance, MockModule, MockProvider, MockRender} from 'ng-mocks';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { DashboardService } from '../../service/dashboard.service';
 import { MaterialModule } from 'src/app/shared/material/material.module';
 import { DashboardModule } from '../../dashboard.module';
+import { of } from 'rxjs';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
-
-  let httpServiceBaseStub;
+  let service: DashboardService;
+  let entradas = [
+    {
+      "nome": "Aluguel",
+      "categoriaId": 1,
+      "data": "17-06-2023",
+      "pago": false,
+      "tipo": "despesa",
+      "valor": "1490",
+      "id": 1
+    },
+    {
+      "nome": "Video game",
+      "categoriaId": 3,
+      "data": "17-04-2023",
+      "pago": true,
+      "tipo": "despesa",
+      "valor": "345",
+      "id": 2
+    },
+    {
+      "nome": "Salario",
+      "categoriaId": 4,
+      "data": "17-04-2023",
+      "pago": true,
+      "tipo": "receita",
+      "valor": "3000",
+      "id": 3
+    },
+    {
+      "nome": "Gas de cozinha",
+      "categoriaId": 1,
+      "data": "17-04-2023",
+      "pago": true,
+      "tipo": "despesa",
+      "valor": "90",
+      "id": 4
+    },
+    {
+      "nome": "Reforma",
+      "categoriaId": 1,
+      "data": "24-04-2023",
+      "pago": true,
+      "tipo": "despesa",
+      "valor": 789,
+      "id": 5
+    }
+  ];
 
   beforeEach(async () => {
-
-    MockBuilder(DashboardComponent).keep(ReactiveFormsModule);
 
     await TestBed.configureTestingModule({
       declarations: [ DashboardComponent ],
       providers:[
         FormBuilder,
-        MockProvider(DashboardService),
+        MockProvider(DashboardService, {
+          getEntradas: () => of(entradas)
+        }),
       ],
       imports:[
         MockModule(ReactiveFormsModule),
-        MockModule(MaterialModule)
+        MockModule(MaterialModule),
       ]
     })
     .compileComponents();
@@ -36,6 +83,8 @@ describe('DashboardComponent', () => {
     component.formDashboard = new FormGroup({
     });
     component.criarFormulario();
+
+    service = TestBed.inject(DashboardService);
     fixture.detectChanges();
   });
 
@@ -50,6 +99,13 @@ describe('DashboardComponent', () => {
     component.getSaldo();
 
     expect(component.saldo).toEqual(4000);
+
+  });
+
+  it('deve buscar os dados de entrada', () => {
+    component.getEntradas();
+
+    expect(component.entradas).toEqual(entradas)
 
   });
 });
