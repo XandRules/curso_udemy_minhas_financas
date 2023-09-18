c212fb6fac2388986b18c4caff449a0624fac8be
XandRules <alexandresilva58@gmail.com>
2023-09-18 19:18:18 -0300

    Add novos testes

--- src/app/commom/auth/components/login/login.component.spec.ts
+++ src/app/commom/auth/components/login/login.component.spec.ts
@@ -2,13 +2,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { MaterialModule } from 'src/app/shared/material/material.module';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [ LoginComponent ],
+      imports: [
+        HttpClientTestingModule,
+        MaterialModule,
+        BrowserAnimationsModule,
+        NoopAnimationsModule
+      ]
     })
     .compileComponents();
 
@@ -19,5 +29,35 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+
+    const button: HTMLButtonElement = fixture.nativeElement.querySelector('button');
+    const email = fixture.nativeElement.querySelector('email');
+    const password = fixture.nativeElement.querySelector('password');
+
+
+    expect(button.disabled).toBe(true);
+
   });
+
+  it('should be enable button', () => {
+
+    const button: HTMLButtonElement = fixture.nativeElement.querySelector('button');
+    const email: HTMLInputElement = fixture.nativeElement.querySelector('#email');
+    const password: HTMLInputElement = fixture.nativeElement.querySelector('#password');
+
+    fixture.detectChanges();
+    email.value = 'alexandre@email.com';
+    password.value = '1234';
+
+    fixture.detectChanges();
+
+    expect(password.value).toBe('1234');
+    expect(email.value).toBe('alexandre@email.com');
+
+    expect(component.loginForm.invalid).toBe(false);
+
+  });
+
+
+
 });
